dfac18eb18be521afa5bd370afc904df8cd48e43
Mike <mike.vercoelen@gmail.com>
2016-02-27 19:24:03 +0100

    now uses glob base to resolve globs, now uses slash for windows problems

--- src/index.js
+++ src/index.js
@@ -2,49 +2,45 @@ import path from 'path'
 import fs from 'fs'
 import through from 'through2'
 import glob from 'glob'
-// import slash from 'slash'
+import slash from 'slash'
 
 export default function gulpSassGlob () {
   return through.obj(transform)
 }
 
 function transform (file, env, callback) {
-  // /@import\s+["']([^"']+\*(\.scss|\.sass)?)["'];?/;
-  const reg = /@import\s+[\"']([^\"']*\*[^\"']*)[\"']/
+  const reg = /@import\s+["']([^"']+\*(\.scss|\.sass)?)["'];?/
   const isSass = path.extname(file.path) === '.sass'
+  const base = path.join(path.normalize(file.base), '/')
 
   let contents = file.contents.toString('utf-8')
 
   let result
 
   while ((result = reg.exec(contents)) !== null) {
-    // const index = result.index
-    const sub = result[0]
-    const globName = result[1]
+    const importRule = result[0]
+    const globPattern = result[1]
 
-    const files = glob.sync(file.base + globName)
-    let replaceString = ''
+    const files = glob.sync(path.join(file.base, globPattern), {
+      cwd: file.base
+    })
+
+    let imports = []
 
     files.forEach((filename) => {
-      if (filename !== file.path) {
-        replaceString += process(filename, isSass)
+      if (filename !== file.path && isSassOrScss(filename)) {
+        // remove parent base path
+        filename = path.normalize(filename).replace(base, '')
+        imports.push('@import "' + slash(filename) + '"' + (isSass ? '' : ';'))
       }
     })
 
-    contents = contents.replace(sub, replaceString)
+    const replaceString = imports.join('\n')
+    contents = contents.replace(importRule, replaceString)
+    file.contents = new Buffer(contents)
   }
 
-  file.contents = new Buffer(contents)
   callback(null, file)
-}
-
-function process (filename, isSass) {
-  if (!isSassOrScss(filename)) {
-    return ''
-  }
-
-  filename = filename.replace(/\\/g, '/')
-  return '@import "' + filename + '"' + (isSass ? '' : ';') + '\n'
 }
 
 function isSassOrScss (filename) {
