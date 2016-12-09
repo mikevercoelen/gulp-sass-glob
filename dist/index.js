110a6d30a9fabd7e265e2a38012116d776f177dd
mike <mike.vercoelen@gmail.com>
2016-12-09 13:22:43 +0100

    Published new version

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
@@ -35,92 +35,90 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 var IMPORT_RE = /^([ \t]*(?:\/\*.*)?)@import\s+["']([^"']+\*[^"']*(?:\.scss|\.sass)?)["'];?([ \t]*(?:\/[\/\*].*)?)$/gm;
 
 function gulpSassGlob() {
-    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-    return _through2.default.obj(function () {
-        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-            args[_key] = arguments[_key];
-        }
+  return _through2.default.obj(function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
 
-        transform.apply(undefined, args.concat([options]));
-    });
+    transform.apply(undefined, args.concat([options]));
+  });
 }
 
 function transform(file, env, callback) {
-    var options = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
+  var options = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
 
-    var includePaths = options.includePaths || [];
-    for (var _i = 0; _i < includePaths.length; _i++) {
-        includePaths[_i] = _path2.default.join(_path2.default.normalize(includePaths[_i]), '/');
-    }
+  var includePaths = options.includePaths || [];
 
-    var isSass = _path2.default.extname(file.path) === '.sass';
-    var base = _path2.default.normalize(_path2.default.join(_path2.default.dirname(file.path), '/'));
+  for (var i = 0; i < includePaths.length; i++) {
+    includePaths[i] = _path2.default.join(_path2.default.normalize(includePaths[i]), '/');
+  }
 
-    var searchBases = [base].concat(_toConsumableArray(includePaths));
-    var contents = file.contents.toString('utf-8');
-    var contentsCount = contents.split('\n').length;
+  var isSass = _path2.default.extname(file.path) === '.sass';
+  var base = _path2.default.normalize(_path2.default.join(_path2.default.dirname(file.path), '/'));
 
-    var result = void 0;
+  var searchBases = [base].concat(_toConsumableArray(includePaths));
+  var contents = file.contents.toString('utf-8');
+  var contentsCount = contents.split('\n').length;
 
-    for (var i = 0; i < contentsCount; i++) {
-        result = IMPORT_RE.exec(contents);
+  var result = void 0;
 
-        if (result !== null) {
-            var files;
+  for (var _i = 0; _i < contentsCount; _i++) {
+    result = IMPORT_RE.exec(contents);
 
-            var _base_path;
+    if (result !== null) {
+      (function () {
+        var _result = result,
+            _result2 = _slicedToArray(_result, 4),
+            importRule = _result2[0],
+            startComment = _result2[1],
+            globPattern = _result2[2],
+            endComment = _result2[3];
 
-            (function () {
-                var _result = result,
-                    _result2 = _slicedToArray(_result, 4),
-                    importRule = _result2[0],
-                    startComment = _result2[1],
-                    globPattern = _result2[2],
-                    endComment = _result2[3];
+        var files = [];
+        var basePath = void 0;
+        for (var _i2 = 0; _i2 < searchBases.length; _i2++) {
+          basePath = searchBases[_i2];
 
-                files = [];
+          files = _glob2.default.sync(_path2.default.join(basePath, globPattern), {
+            cwd: basePath
+          });
 
-                for (var _i2 = 0; _i2 < searchBases.length; _i2++) {
-                    _base_path = searchBases[_i2];
-
-                    files = _glob2.default.sync(_path2.default.join(_base_path, globPattern), {
-                        cwd: _base_path
-                    });
-                    if (files.length > 0) {
-                        break;
-                    }
-                }
-
-                var imports = [];
-
-                files.forEach(function (filename) {
-                    if (filename !== file.path && isSassOrScss(filename)) {
-                        // remove parent base path
-                        filename = _path2.default.normalize(filename).replace(_base_path, '');
-                        imports.push('@import "' + (0, _slash2.default)(filename) + '"' + (isSass ? '' : ';'));
-                    }
-                });
-
-                if (startComment) {
-                    imports.unshift(startComment);
-                }
-
-                if (endComment) {
-                    imports.push(endComment);
-                }
-
-                var replaceString = imports.join('\n');
-                contents = contents.replace(importRule, replaceString);
-                file.contents = new Buffer(contents);
-            })();
+          if (files.length > 0) {
+            break;
+          }
         }
-    }
 
-    callback(null, file);
+        var imports = [];
+
+        files.forEach(function (filename) {
+          if (filename !== file.path && isSassOrScss(filename)) {
+            // remove parent base path
+            filename = _path2.default.normalize(filename).replace(basePath, '');
+            imports.push('@import "' + (0, _slash2.default)(filename) + '"' + (isSass ? '' : ';'));
+          }
+        });
+
+        if (startComment) {
+          imports.unshift(startComment);
+        }
+
+        if (endComment) {
+          imports.push(endComment);
+        }
+
+        var replaceString = imports.join('\n');
+        contents = contents.replace(importRule, replaceString);
+        file.contents = new Buffer(contents);
+      })();
+    }
+  }
+
+  callback(null, file);
 }
 
 function isSassOrScss(filename) {
-    return !_fs2.default.statSync(filename).isDirectory() && _path2.default.extname(filename).match(/\.sass|\.scss/i);
+  return !_fs2.default.statSync(filename).isDirectory() && _path2.default.extname(filename).match(/\.sass|\.scss/i);
 }
 module.exports = exports['default'];
